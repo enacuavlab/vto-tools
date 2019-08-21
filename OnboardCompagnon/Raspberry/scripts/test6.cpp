46ca89bab748b20f6965dfdf16b2e0bd1d131814
pprz <xp31@free.fr>
2019-08-21 14:09:31 +0200

    run Ok

--- OnboardCompagnon/Raspberry/scripts/test6.cpp
+++ OnboardCompagnon/Raspberry/scripts/test6.cpp
@@ -3,7 +3,6 @@
 #include <mutex>
 #include <thread>
 #include <condition_variable>
-
 #include <chrono>
 
 /*
@@ -13,6 +12,7 @@ g++ -g test6.cpp -o test6 `pkg-config --cflags --libs opencv` -D_GLIBCXX_USE_CXX
 using namespace cv;
 using namespace std;
 
+
 /*****************************************************************************/
 std::mutex mtx;
 std::condition_variable cond;
@@ -21,12 +21,13 @@ bool ready = false;
 bool readyDetect = false;
 
 Rect detect;
+unsigned int fps = 0;
 
 /*****************************************************************************/
 void image_processing(void)
 {
   double scale=1;
-  CascadeClassifier cascade("/opt/opencv-4.1.0/share/opencv4/haarcascades/haarcascade_frontalcatface.xml");
+  CascadeClassifier cascade("/opt/opencv-4.1.0/share/opencv4/haarcascades/haarcascade_frontalface_default.xml");
   vector<Rect> faces;
   Mat gray;
   Mat img1(480,640,CV_8UC3,Scalar(0,0,0));
@@ -37,16 +38,16 @@ void image_processing(void)
 
     cvtColor(frameIn,img1,COLOR_YUV2BGR_I420);
     cvtColor(img1,gray,COLOR_BGR2GRAY);
-    imwrite("img.jpg",gray);
+    cascade.detectMultiScale(gray, faces, 2.0, 1, 0|CASCADE_SCALE_IMAGE, Size(80, 80) );
 
-    cascade.detectMultiScale(gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    cout << "faces.size() " << faces.size() << endl;
-
-    if(faces.size()==1) {
+    if(faces.size()>0) {
       detect = faces[0];
       readyDetect = true;
+    } else {
+      readyDetect = false;
     }
 
+    fps++;
     ready = false;
     lck.unlock();
   }
@@ -54,8 +55,6 @@ void image_processing(void)
 
 /*****************************************************************************/
 unsigned int width=640,height=480;
-//unsigned int ax=220,bx=320;
-//unsigned int ay=210,by=300;
 void y_overlay(Mat *param)
 {
   unsigned int ax=detect.x,bx=ax+detect.width;
@@ -75,10 +74,19 @@ void y_overlay(Mat *param)
 }
 
 /*****************************************************************************/
+void fps_processing(void) 
+{
+  while(true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//    cout << fps << endl;
+    fps=0;
+  }
+}
+
+/*****************************************************************************/
 void stream_processing(void) 
 {
-  unsigned int cpt = 0;
-
+  unsigned int dataInSize = sizeof(unsigned char)*640*480*3/2;
   Mat dataIn(480*3/2,640,CV_8UC1);
   Mat dataOut(480,640,CV_8UC3,Scalar(0,0,0));
 
@@ -98,22 +106,17 @@ void stream_processing(void)
     while(true) {
       in.read(dataIn);
       if (!dataIn.empty()) { 
-
         if(!ready) {
           std::unique_lock<std::mutex> lck(mtx);
-          memcpy(frameIn.data, dataIn.data, sizeof(unsigned char)*640*480*3/2);  
+          memcpy(frameIn.data, dataIn.data, dataInSize);
           ready=true;
           lck.unlock();
           cond.notify_one();
 	}
 
-        memcpy(dataOut.data, dataIn.data, sizeof(unsigned char)*640*480*3/2);  
+        memcpy(dataOut.data, dataIn.data, dataInSize);
         if(readyDetect) y_overlay(&dataOut);
         out.write(dataOut);
-
-        cpt += 1;
-        cout << "processing " << cpt << endl;
-
       }
     }
   }
@@ -125,8 +128,10 @@ void stream_processing(void)
 /*****************************************************************************/
 int main(int, char**)
 {
+  thread t0(fps_processing);
   thread t1(stream_processing);
   thread t2(image_processing);
+  t0.join();
   t1.join();
   t2.join();
 
