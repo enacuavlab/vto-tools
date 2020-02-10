3280c411bdd4b7b0eb5e4dc00a788f1d39855257
pprz <xp31@free.fr>
2020-02-10 15:44:00 +0100

    tested

--- OnboardCompagnon/Raspberry/scripts/Material/cv.cpp
+++ OnboardCompagnon/Raspberry/scripts/Material/cv.cpp
@@ -2,71 +2,61 @@
 #include <opencv2/opencv.hpp>
 #include <pthread.h>
 
-#define SCALE 3/2
-#define CAMOUTSTR "/tmp/camera3"
-
 using namespace cv;
 using namespace std;
 
-
+/*****************************************************************************/
+#define SAD_LIMIT 150
+#define SCALE 3/2
+#define streamOutGstStr "appsrc ! shmsink socket-path=/tmp/camera3 wait-for-connection=false async=false sync=false"
 
 /*****************************************************************************/
-struct pthreadarg_t {
-  int width;
-  int height;
-  int mbx;
-  int mby;
-  int tabsize;
-} pthreadarg_t;
-
 struct motion_elt_t {
  int8_t x;
  int8_t y;
  uint16_t sad;
 };
-motion_elt_t *motionTabIn;
+static motion_elt_t *motionIn;
+static Mat imageIn;
+static int width,height,fps;
 
 static pthread_mutex_t imv_mutex;
-bool imv_ready=false;
+static bool imv_ready=false;
 
 static pthread_t img_thread;
 static pthread_mutex_t img_mutex;
 static pthread_cond_t img_condv;
-bool img_ready=false;
+static bool img_ready=false;
 
-bool init_ready=false;
-
-Mat imageIn;
-VideoWriter streamOut;
-
-static int8_t mline[8][8] = { \
-  {1, 1, 1, 1, 1, 1, 1, 1 }, 
-  {1, 1, 1, 1, 2, 2, 2, 2 }, 
-  {1, 1, 2, 2, 2, 2, 3, 3 }, 
-  {1, 1, 2, 2, 3, 3, 3, 4 }, 
-  {1, 2, 2, 3, 3, 4, 4, 5 }, 
-  {1, 2, 2, 3, 4, 5, 5, 6 }, 
-  {1, 2, 3, 4, 5, 5, 6, 7 }, 
-  {1, 2, 3, 4, 5, 6, 7, 8 } };
+static bool init_ready=false;
 
 /*****************************************************************************/
 static void *process_thread(void *ptr)
 {
-  struct pthreadarg_t args;
-  memcpy(&args,ptr,sizeof(pthreadarg_t));
+  VideoWriter strOut = VideoWriter(streamOutGstStr,0,fps/1,Size(width,height),true);
 
-  unsigned int imageSize = ((args.width * args.height * sizeof(uint8_t))*SCALE);
-  Mat imageOut(args.width, args.height, CV_8UC3,Scalar(0,0,0));
+  unsigned int imageSize = ((width * height * sizeof(uint8_t))*SCALE);
+  Mat imageOut(width, height, CV_8UC3,Scalar(0,0,0));
+  char* imgptr = imageOut.ptr<char>(0);
 
-  unsigned int motionSize = (args.tabsize)*sizeof(motion_elt_t);
-  motion_elt_t *motionTabOut = new motion_elt_t[args.tabsize];
+  int mbx = width/16;
+  int mby = height/16;
+  unsigned int motionSize = ((mbx+1)*mby) * sizeof(struct motion_elt_t); 
+  motion_elt_t *motionOut = new motion_elt_t[(mbx+1)*mby];
 
-  unsigned int uoffset=(args.width * args.height);
+  unsigned int uoffset=(width * height);
   unsigned int voffset=uoffset*5/4;
 
-  unsigned int i,j,k,l,m,n,s;
+  unsigned int k,l,m,s;
+  int x,y,p,q,a,b;
   int8_t r;
-  int x,y,p,q;
+
+  int8_t mline[17][17];
+  for(int y=0;y<=16;y++) {
+    for(int x=0;x<=16;x++) {
+      mline[x][y]=floor((x*y/16.0)+0.5);
+    }
+  }
 
   while (true) {
 
@@ -78,76 +68,71 @@ static void *process_thread(void *ptr)
 
     if(imv_ready) { 
       pthread_mutex_lock(&imv_mutex);
-      memcpy(motionTabOut ,motionTabIn, motionSize);
+      memcpy(motionOut ,motionIn, motionSize);
       imv_ready=false;
       pthread_mutex_unlock(&imv_mutex);
     }
 
-    for (j = 0; j < args.mby; j++) {
-      for (i = 0; i < (args.mbx); i++) {
+    for (int j=1;j<(mby-1);j++) {
+      for (int i=1;i< (mbx-1);i++) {
 
 	x=((i-1)*8)+12;
         y=((j-1)*8)+12;
-        k=(2*((y*(args.mbx)*16)+x));
-        imageOut.data[k] = 0;
+        k=(2*((y*(mbx)*16)+x));
+	imgptr[k]=0;
 
-        l=(y*(args.mbx)*8)+x;
+        l=(y*(mbx)*8)+x;
         m = (l & 1) ? 0xFF : 0x00;
-        imageOut.data[uoffset+l] = imageOut.data[uoffset+l] & m;
-        imageOut.data[voffset+l] = imageOut.data[voffset+l] & m;
+        imgptr[uoffset+l] = imgptr[uoffset+l] & m;
+        imgptr[voffset+l] = imgptr[voffset+l] & m;
 
+        motion_elt_t *vec = motionOut + (i+(mbx+1)*j);
 
-        motion_elt_t *vec = motionTabOut + (i+(args.mbx+1)*j);
         if (vec->x == 0 && vec->y == 0) continue;
-//      if (vec->sad > sad_limit) continue;
+        if (vec->sad > SAD_LIMIT) continue;
 
-        p = ((vec->x)>>3);if(p>8)p=8;
-        q = ((vec->y)>>3);if(q>8)q=8;
- 
-        p=8;q=8;
+        p = ((vec->x)>>2);
+	if(p<0) {a=-1;p=-p;} else a=1;
+	if(p>15) p=15;
+        q = ((vec->y)>>2);
+	if(q<0) {b=-1;q=-q;} else b=1;
+	if(q>15) q=15;
 
- 	if((q!=0)||(p!=0)) {
-	  if((q<9)&&(p<9)) {
-            for (n = 0; n < p; n++) {
-              r=mline[n][q]; 
-              s=r;
-cout << +(s) << endl;
-              imageOut.data[k+(r*(n+1))+(r-1)*(args.width)]=0;
-	    }
+        if(p<q) {
+          for (int n=0;n<=q;n++) {
+            r=mline[n][p]; 
+	    s=(b*n*width)-(a*r);
+            imgptr[k+s]=0;
+	  }
+	} else {
+          for (int n=0;n<=p;n++) {
+            r=mline[n][q];
+	    s=(b*r*width)-(a*n);
+            imgptr[k+s]=0;
 	  }
 	}
       }
     }
-
-    streamOut.write(imageOut);
+    strOut.write(imageOut);
   }
   return((void *)0);
 }
 
 
 /*****************************************************************************/
-void cv_init(int width, int height, int fps, int fmt)
+void cv_init(int w, int h, int f, int fmt)
 {
-  streamOut = VideoWriter(
-    "appsrc ! "
-    "shmsink socket-path=" CAMOUTSTR
-    " wait-for-connection=false async=false sync=false",
-    0,fps/1,Size(width,height),true);
+  width=w;height=h;fps=f;
 
   imageIn.create(width*SCALE, height, CV_8UC1);
-
-  int mbx = width/16;
-  int mby = height/16;
-  int motionTabSize = (mbx+1) * mby;
-  motionTabIn = new motion_elt_t[motionTabSize];
+  motionIn = new motion_elt_t[((width/16)+1) * (height/16)];
 
   pthread_mutex_init(&imv_mutex, NULL);
 
   pthread_mutex_init(&img_mutex, NULL);
   pthread_cond_init(&img_condv, NULL);
 
-  struct pthreadarg_t param = {width,height,mbx,mby,motionTabSize};
-  pthread_create(&img_thread, NULL, process_thread, (void *)&param);
+  pthread_create(&img_thread, NULL, process_thread, (void *)0);
 
   init_ready=true;
 }
@@ -171,7 +156,7 @@ void cv_process_imv(uint8_t *p_buffer, int length, int64_t timestamp)
 {
   if (init_ready) {
     pthread_mutex_lock(&imv_mutex);
-    memcpy(motionTabIn ,p_buffer, length);
+    memcpy(motionIn ,p_buffer, length);
     imv_ready=true;
     pthread_mutex_unlock(&imv_mutex);
   }
