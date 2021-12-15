9e2083d34d603104e79b80110ed5b90bd488d126
xp31 <xp31@free.fr>
2021-12-15 14:21:06 +0100

    up

--- pprz-motive/monitor.py
+++ pprz-motive/monitor.py
@@ -8,7 +8,7 @@ import time,sys,argparse
 import tkinter as tk
 import numpy as np
 from scipy.spatial.transform import Rotation
-from threading import Timer, Thread
+from threading import Timer, Thread, Event
 
 
 class Timer(Timer):
@@ -30,30 +30,51 @@ class Gui(tk.Tk):
   def close():
     self.destroy()
 
-  def tick(self,i):
+  def update_lab1(self,string):
     try:
-      self.lab1['text'] = i.string1
-      self.lab2['text'] = i.string2
+      self.lab1['text'] = string
+    except RuntimeError:
+      exit()
+
+  def update_lab2(self,string):
+    try:
+      self.lab2['text'] = string
     except RuntimeError:
       exit()
 
 
-class Proc(Thread):
-  def __init__(self, t, tole):
+class Compute1(Thread):
+  def __init__(self,g,i):
+    Thread.__init__(self)
+    self.g = g
+    self.i = i
 
   def run(self):
-    cpt=0
-    while True:
-      line=sys.stdin.readline()
-    pos = (np.fromiter(data[:1][0].split(','),dtype=float))
-    spd = (np.fromiter(data[1:2][0].split(','),dtype=float))
-    rot = Rotation.from_quat(np.fromiter(data[2:3][0].split(','),dtype=float))
-    rot_euler = rot.as_euler('xyz', degrees=True)
-    psi = rot_euler[0]-180 if rot_euler[0] > 0 else 180+rot_euler[0] # YAW
-    phi = -1*rot_euler[1] # ROLL 
-    theta = rot_euler[2]  # PITCH
-    self.string1 = (f'{int(psi):+04}',f'{int(phi):+04}',f'{int(theta):+04}')
-    self.valid = True
+    while self.i.available_string.wait():
+      data = self.i.string
+      self.i.available_string.clear()
+      pos = (np.fromiter(data[:1][0].split(','),dtype=float))
+      spd = (np.fromiter(data[1:2][0].split(','),dtype=float))
+      rot = Rotation.from_quat(np.fromiter(data[2:3][0].split(','),dtype=float))
+      rot_euler = rot.as_euler('xyz', degrees=True)
+      psi = rot_euler[0]-180 if rot_euler[0] > 0 else 180+rot_euler[0] # YAW
+      phi = -1*rot_euler[1] # ROLL 
+      theta = rot_euler[2]  # PITCH
+      tmp = (f'{int(psi):+04}',f'{int(phi):+04}',f'{int(theta):+04}')
+      g.update_lab1(tmp)
+
+
+class Compute2(Thread):
+  def __init__(self,g,i):
+    Thread.__init__(self)
+    self.g = g
+    self.i = i
+
+  def run(self):
+    while self.i.available_outcpt.wait():
+      data = self.i.outcpt
+      self.i.available_outcpt.clear()
+      g.update_lab2(data)
 
 
 class Input(Thread):
@@ -61,11 +82,11 @@ class Input(Thread):
     Thread.__init__(self)
     self.t = t
     self.tole = tole
-    self.string1 = ''
-    self.string2 = ''
+    self.string = ''
     self.stamp = 0.
-    self.valid = False
     self.outcpt = 0
+    self.available_string = Event()
+    self.available_outcpt = Event()
 
   def run(self):
     cpt=0
@@ -73,20 +94,22 @@ class Input(Thread):
       line=sys.stdin.readline()
       words = line.split()
       if(len(words[6:][2:3])==0): continue
-      #if(cpt==0):cpt=1;self.t.start()
+      if(cpt==0):cpt=1;self.t.start()
       tmp = time.time()
       if int(self.stamp)!= 0: 
         delay = tmp - self.stamp 
-        if (delay>=self.tole):self.outcpt=self.outcpt+1
+        if (delay>=self.tole):
+          self.outcpt=self.outcpt+1
+          self.available_outcpt.set()
       self.stamp = tmp
-      self.procline(words[6:])
+      self.string = words[6:]
+      self.available_string.set()
 
   def tick(self):
-    delta = (time.time()-self.stamp)
-    if delta > 1: self.cpt = self.cpt+1
-    self.string2 = str(self.cpt)
-    print(self.valid)
-
+    delay=time.time()-self.stamp
+    if (delay>=self.tole):
+      self.outcpt=self.outcpt+1
+      self.available_outcpt.set()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -96,12 +119,16 @@ if __name__ == '__main__':
 
   def tickcb():
     i.tick()
-    g.tick(i)
-    i.string1 = "XXXXXXXXXXXXX"
 
   g = Gui()
   t = Timer(1/args.freq,tickcb)
   i = Input(t,(1.0/args.freq)*(100.+args.tole)/100.)
+  c1 = Compute1(g,i)
+  c2 = Compute2(g,i)
+  c1.start()
+  c2.start()
   i.start()
   g.mainloop()
   i.join()
+  c1.join()
+  c2.join()
