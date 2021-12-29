9b3dc12222c71c2c374a93a3c783e40c3b1e6dfa
amtcvx <xp31@free.fr>
2021-12-29 12:07:51 +0100

    up

---
+++ Android/readme.txt
@@ -0,0 +1,34 @@
+Google play android application:
+HyperImu
+Sebastiano Campisi v3.1.3.0
+
+socat - udp-recv:5555
+#socat - udp-recvfrom:5555,fork
+#socat - TCP4-LISTEN:5555
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------
+Google play android application:
+Sensorstream IMU+GPS
+Axel Lorenz
+
+- set target IP address
+- set UDP stream 
+- switch stream 
+- set sensors
+
+socat - udp-recvfrom:5555,fork
---
+++ Android/test.c
@@ -0,0 +1,76 @@
+/*
+gcc test.c -lpthread -o test
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/time.h>
+
+
+#define DATAPORT 5555
+
+#define MAX_PACKETSIZE 1024
+
+
+int createsocketdata() {
+  int sockfd = -1;
+  int one=1;
+
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) exit(-1);
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one))  < 0) exit(-1);
+
+  struct sockaddr_in my_addr;
+  memset(&my_addr, 0, sizeof(my_addr));
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(DATAPORT);
+  my_addr.sin_addr.s_addr = INADDR_ANY;
+  if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) < 0) exit(-1); 
+
+  return(sockfd);
+}
+
+
+void* recvloop(void *arg) {
+  int rcv;
+  struct sockaddr_in their;
+  int addr_len;
+  char buf[MAX_PACKETSIZE];
+  int fd = *((int*)arg);
+  struct timeval tv,tv_st,tv_res;
+  float ugyr[3],uacc[3],gyr[3],acc[3];
+
+  timerclear(&tv_st);
+  while(1) {
+    rcv = recvfrom(fd,(char *)buf,MAX_PACKETSIZE,0,(struct sockaddr *)&their,(socklen_t*)&addr_len);
+    if(rcv>0) {
+      gettimeofday(&tv,NULL);
+      if (timerisset(&tv_st)){
+        timersub(&tv,&tv_st,&tv_res);
+        printf("sub %ld.%06ld\n",tv_res.tv_sec,tv_res.tv_usec);
+      }
+      memcpy(&tv_st,&tv,sizeof(tv));
+      printf("%s\n",buf);
+      sscanf(buf,"%f,%f,%f,%f,%f,%f",&ugyr[0],&ugyr[1],&ugyr[2],&uacc[0],&uacc[1],&uacc[2]);
+      printf("%f %f %f %f %f %f\n",ugyr[0],ugyr[1],ugyr[2],uacc[0],uacc[1],uacc[2]);
+    }
+  }
+  return NULL;
+}
+
+
+int main( int argc, char*argv[]) {
+  int gDatSock=-1;
+  pthread_t gDatThr;
+
+  gDatSock = createsocketdata();
+  if (pthread_create(&gDatThr, NULL, &recvloop, &gDatSock) <0)  exit(-1);
+  pthread_join(gDatThr,NULL);
+  return(0);
+}
