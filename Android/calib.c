e88d2415c9a11e94516576dd67ff1c75a63d1f06
amtcvx <xp31@free.fr>
2022-01-01 18:20:01 +0100

    up

--- Android/calib.c
+++ Android/calib.c
@@ -1,52 +1,108 @@
 /*
-gcc calib.c -o calib
-
+gcc -g calib.c -lm -o calib
 */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define ACCNOISEPERCENT 0.1 // Below 10%
+#define MAGNOISEPERCENT 0.6 // Below 60%
+#define ACCNOISEWINDOW	20
+#define MAGNOISEWINDOW	10
 
 #define STRSIZE 500
 
 typedef struct elt_t elt_t;
-typedef struct elt_t {
-  int val;
+struct elt_t {
+  unsigned int stamp;
+  float val;
+  bool filtered;
   struct elt_t *nxt;
-} *llist;
+};
 
 
 int main( int argc, char*argv[]){
   char buf[STRSIZE];
+  int cpt=0;
 
   if(argc<=1) exit(-1);
   FILE *in=fopen(argv[1],"r");
   if(in!=NULL){
+    elt_t* elt=malloc(sizeof(elt_t));
+    elt->nxt=(struct elt_t*)0;
+    elt_t* first=elt;
     while(fgets(buf, STRSIZE, in) != NULL){
-      elt_t* elt = malloc(sizeof(elt_t));
-     
-/*
-      ptr = malloc &samples[cpt];
-      bzero(ptr,sizeof(struct sample_t));
-      sscanf(buf,"%f,%f,%f,%f,%f,%f,%f,%f,%f",&(ptr->ugyr[0]),&(ptr->ugyr[1]),&(ptr->ugyr[2]),\
-        &(ptr->uacc[0]),&(ptr->uacc[1]),&(ptr->uacc[2]),&(ptr->umag[0]),&(ptr->umag[1]),&(ptr->umag[2]));
-      printf("%f %f %f %f %f %f %f %f %f\n",ptr->ugyr[0],ptr->ugyr[1],ptr->ugyr[2],\
-	ptr->uacc[0],ptr->uacc[1],ptr->uacc[2],ptr->umag[0],ptr->umag[1],ptr->umag[2]);
+      sscanf(buf,"%d %f",&(elt->stamp),&(elt->val));
+      elt->filtered=false;
+      (elt->nxt)=malloc(sizeof(elt_t));
+      (elt->nxt->nxt)=(struct elt_t*)0;
+      elt=elt->nxt;
+    }
+
+    // Compute mean
+    elt=first;
+    float neutral=0.;
+    cpt=0;
+    while(elt->nxt!=NULL){
+      neutral+=(elt->val);
+      elt=elt->nxt;
       cpt++;
-*/
     }
-/*
-    for(int i=0;i<cpt;i++) {
-      ptr = &samples[i];
-      for(int j=0;j<3;j++) { 
-        bias.ugyr[j]+=ptr->ugyr[j];bias.uacc[j]+=ptr->uacc[j];bias.umag[j]+=ptr->umag[j];
+    neutral/=cpt;
+    //printf("neutral:%f\n",neutral);
+
+    // Compute median L2 norm
+    elt=first;
+    float median=0.;
+    cpt=0;
+    while(elt->nxt!=NULL){
+      median+=sqrt(((elt->val)-neutral)*((elt->val)-neutral));
+      elt=elt->nxt;
+      cpt++;
+    }
+    printf("median:%f\n",median);
+
+
+    // Filter noisy within a sliding window
+    float noise_threshold=median*ACCNOISEPERCENT;
+    float noise=0.;
+    bool loop=true;
+    elt_t* eltin;
+    elt=first;
+    while(loop){
+      cpt=0;
+      noise=0.;
+      eltin=elt;
+      while((cpt<ACCNOISEWINDOW)&&loop){
+        noise+=((eltin->val)*(eltin->val));
+        eltin=eltin->nxt;
+	if(eltin==NULL)loop=false;
+	cpt++;
       }
+      noise=sqrt(noise)/cpt;
+      if(noise<noise_threshold){
+	cpt=0;
+	loop=true;
+        eltin=elt;
+        while((cpt<ACCNOISEWINDOW)&&loop){
+          eltin->filtered=true;
+          eltin=eltin->nxt;
+	  if(eltin==NULL)loop=false;
+	  cpt++;
+	}
+      }
+      elt=elt->nxt;
     }
-    cpt--;
-    for(int j=0;j<3;j++) { 
-      bias.ugyr[j]=bias.ugyr[j]/cpt;bias.uacc[j]=bias.uacc[j]/cpt;bias.umag[j]=bias.umag[j]/cpt;
+
+    // Display
+/* 
+    elt=first;
+    while(elt->nxt!=NULL){
+      printf("%d %d %d %f\n",true,(elt->stamp),(elt->filtered),(elt->val));
+      elt=elt->nxt;
     }
-    printf("\n\n%f %f %f %f %f %f %f %f %f\n",bias.ugyr[0],bias.ugyr[1],bias.ugyr[2],\
-      bias.uacc[0],bias.uacc[1],bias.uacc[2],bias.umag[0],bias.umag[1],bias.umag[2]);
 */
   }
   return(0);
