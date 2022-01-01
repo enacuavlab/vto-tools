54e6746f3ab2752b852453a01ee80d7fabb768d7
amtcvx <xp31@free.fr>
2022-01-01 10:57:02 +0100

    up

--- Android/calib2.c
+++ Android/calib2.c
@@ -6,35 +6,36 @@ gcc calib.c -o calib
 #include <string.h>
 #include <stdlib.h>
 
-#define STRSIZE 500
+#define STRSIZE 150
 
-typedef struct elt_t elt_t;
-typedef struct elt_t {
-  int val;
-  struct elt_t *nxt;
-} *llist;
+struct sample_t {
+  float ugyr[3];
+  float uacc[3];
+  float umag[3];
+};
 
+#define MAXSAMPLES 5000
+struct sample_t samples[MAXSAMPLES];
 
-int main( int argc, char*argv[]){
+int main( int argc, char*argv[]) {
+  struct sample_t bias;
+  struct sample_t *ptr;
+  int cpt=0;
   char buf[STRSIZE];
+  FILE *in;
 
   if(argc<=1) exit(-1);
-  FILE *in=fopen(argv[1],"r");
-  if(in!=NULL){
+  in = fopen(argv[1],"r");
+  if(in!=NULL) {
     while(fgets(buf, STRSIZE, in) != NULL){
-      elt_t* elt = malloc(sizeof(elt_t));
-     
-/*
-      ptr = malloc &samples[cpt];
+      ptr = &samples[cpt];
       bzero(ptr,sizeof(struct sample_t));
       sscanf(buf,"%f,%f,%f,%f,%f,%f,%f,%f,%f",&(ptr->ugyr[0]),&(ptr->ugyr[1]),&(ptr->ugyr[2]),\
         &(ptr->uacc[0]),&(ptr->uacc[1]),&(ptr->uacc[2]),&(ptr->umag[0]),&(ptr->umag[1]),&(ptr->umag[2]));
       printf("%f %f %f %f %f %f %f %f %f\n",ptr->ugyr[0],ptr->ugyr[1],ptr->ugyr[2],\
 	ptr->uacc[0],ptr->uacc[1],ptr->uacc[2],ptr->umag[0],ptr->umag[1],ptr->umag[2]);
       cpt++;
-*/
     }
-/*
     for(int i=0;i<cpt;i++) {
       ptr = &samples[i];
       for(int j=0;j<3;j++) { 
@@ -47,7 +48,7 @@ int main( int argc, char*argv[]){
     }
     printf("\n\n%f %f %f %f %f %f %f %f %f\n",bias.ugyr[0],bias.ugyr[1],bias.ugyr[2],\
       bias.uacc[0],bias.uacc[1],bias.uacc[2],bias.umag[0],bias.umag[1],bias.umag[2]);
-*/
   }
+  printf("%d\n",cpt);
   return(0);
 }
--- Android/readme.txt
+++ Android/readme.txt
@@ -13,8 +13,8 @@ stdbuf -oL -eL socat - udp-recv:5555 | tee 5ms_sample.log
 
 sudo apt-get install feedgnuplot
 
-stdbuf -oL -eL socat - udp-recv:5555 | awk -F "[ ,]" -W interactive '{print systime()" "$1}'  | feedgnuplot --stream --exit --domain --timefmt %s --lines
+stdbuf -oL -eL socat - udp-recv:5555 | awk -F "[ ,]" -W interactive -v start="$(date +%s%3N)" '{cmd="(date +'%s%3N')";cmd | getline d;print d-start,$1;close(cmd)}' | feedgnuplot --stream 0.01 --exit --domain --lines --xlen 10000
 
-stdbuf -oL -eL socat - udp-recv:5555 | awk -F "[ ,]" -W interactive '{cmd="(date +'%s%3N')";cmd | getline d;print d,$1;close(cmd)}'| feedgnuplot --stream --exit --domain --timefmt %s --lines
+stdbuf -oL -eL socat - udp-recv:5555 | awk -F "[ ,]" -W interactive -v start="$(date +%s%3N)" '{cmd="(date +'%s%3N')";cmd | getline d;print d-start,$1;close(cmd)}' | tee 5ms_accelcalibpurpose.log | feedgnuplot --stream 0.01 --exit --domain --lines --xlen 10000
 
-stdbuf -oL -eL socat - udp-recv:5555 | awk -F "[ ,]" -W interactive '{cmd="(date +'%s%3N')";cmd | getline d;print d,$1;close(cmd)}'| feedgnuplot --stream --exit --domain --lines
+feedgnuplot --domain --lines 5ms_accelcalibpurpose.log
