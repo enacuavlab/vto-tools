3433bf4945667d14299c0a91a60619a73b4aa5ce
amtcvx <xp31@free.fr>
2022-02-24 16:59:44 +0100

    up

--- pprz-motive/python/ivy_2_udp.py
+++ pprz-motive/python/ivy_2_udp.py
@@ -17,16 +17,26 @@ IVYBUS = '127:2010'
 UDP_IP='127.0.0.1'
 UDP_PORT=5558
 
+I2P = 1. / 2**8    # integer to position
+#I2V = 1. / 2**19   # integer to velocity
+I2W = 1. / 2**12   # integer to angle
+
 class IvyThread:
   def __init__(self,bus):
     self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM )
-    self.dict_pos = {}
-    self.dict_att = {}
+    self.dict_plane_attitude = {}
+    self.dict_plane_estimator = {}
+    self.dict_plane_navigation = {}
     logging.getLogger('Ivy').setLevel(logging.ERROR)
     readymsg = '%s READY' % IVYAPPNAME
     IvyInit(IVYAPPNAME,readymsg,0,self.on_cnx,0)
     IvyStart(bus)
-    IvyBindMsg(self.on_nps_motion, '([0-9]* NPS_MOTION.*)')
+    IvyBindMsg(self.on_plane_attitude, '([0-9]+ ATTITUDE .*)')
+    IvyBindMsg(self.on_plane_estimator, '([0-9]+ ESTIMATOR .*)')
+    IvyBindMsg(self.on_plane_navigation, '([0-9]+ NAVIGATION .*)')
+    IvyBindMsg(self.on_rotorcraft_fp, '([0-9]+ ROTORCRAFT_FP .*)')
+
+#    IvyBindMsg(self.on_navigation, '([0-9]* NAVIGATION.*)')
 #   IvyBindMsg(self.on_nps_speed_pos, '([0-9]* NPS_SPEED_POS.*)')
 #   IvyBindMsg(self.on_nps_rate_attitude, '([0-9]* NPS_RATE_ATTITUDE.*)')
 
@@ -60,28 +70,67 @@ class IvyThread:
 #      data+=b'\n'
 #      self.sock.sendto(data,(UDP_IP,UDP_PORT))
 #      print(msg)
+#
+#  def on_nps_motion(self, *larg):
+#    posf = np.zeros(3);quatf = np.zeros(4)
+#    words=(larg[1].split())
+#    posf[0]=-float(words[3])
+#    posf[1]=-float(words[2])
+#    posf[2]=-float(words[4])
+#    psi=float(words[5])
+#    phi=-float(words[6])
+#    theta=-float(words[7])
+#    rot = Rotation.from_euler('xyz',[phi,theta,psi], degrees=True)
+#    quatf=rot.as_quat()
+#    msg="ivy "+words[0]+" "
+#    msg+=(" ".join(["%.3f" % nb for nb in posf]))
+#    msg+=" "+(" ".join(["%.6f" % nb for nb in quatf]))
+#    data=msg.encode( 'utf-8' )
+#    data+=b'\n'
+#    self.sock.sendto(data,(UDP_IP,UDP_PORT))
+#    print(msg)
 
   def on_cnx(self, dum1, dum2):
     print(dum1,dum2)
 
-  def on_nps_motion(self, *larg):
+  def on_rotorcraft_fp(self, *larg):
     posf = np.zeros(3);quatf = np.zeros(4)
     words=(larg[1].split())
-    posf[0]=-float(words[3])
-    posf[1]=-float(words[2])
-    posf[2]=-float(words[4])
-    psi=float(words[5])
-    phi=-float(words[6])
-    theta=-float(words[7])
-    rot = Rotation.from_euler('xyz',[phi,theta,psi], degrees=True)
-    quatf=rot.as_quat()
-    msg="ivy "+words[0]+" "
+    posf = [I2P*float(i) for i in words[2:5]]
+    quatf = [I2W*np.rad2deg(float(i)) for i in words[8:11]]
+    print(words[8:11])
+    print(quatf)
+
+  def on_plane_attitude(self, *larg):
+    words=(larg[1].split())
+    self.dict_plane_attitude[int(words[0])]=words[2:5]
+    self.check_plane(int(words[0]))
+
+  def on_plane_estimator(self, *larg):
+    words=(larg[1].split())
+    self.dict_plane_estimator[int(words[0])]=words[2]
+    self.check_plane(int(words[0]))
+
+  def on_plane_navigation(self, *larg):
+    words=(larg[1].split())
+    self.dict_plane_navigation[int(words[0])]=words[4:6]
+    self.check_plane(int(words[0]))
+
+  def check_plane(self, key):
+    if ((key in self.dict_plane_attitude) and (key in self.dict_plane_estimator) and (key in self.dict_plane_navigation)):
+      posf = [-float(i) for i in self.dict_plane_navigation[key]]
+      posf.append(float(self.dict_plane_estimator[key]))
+      angles = [np.rad2deg(float(i)) for i in self.dict_plane_attitude[key]]
+      self.build_snd(key,posf,Rotation.from_euler('xzy',[-angles[2],angles[0],-angles[1]], degrees=True).as_quat())
+      self.dict_plane_attitude.pop(key);self.dict_plane_estimator.pop(key);self.dict_plane_navigation.pop(key)
+
+  def build_snd(self,acid,posf,quatf):
+    msg="ivy "+str(acid)+" "
     msg+=(" ".join(["%.3f" % nb for nb in posf]))
     msg+=" "+(" ".join(["%.6f" % nb for nb in quatf]))
     data=msg.encode( 'utf-8' )
     data+=b'\n'
-    self.sock.sendto(data,(UDP_IP,UDP_PORT))
-    print(msg)
+#    self.sock.sendto(data,(UDP_IP,UDP_PORT))
 
 
 if __name__ == '__main__':
